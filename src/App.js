209002b3d7ddfe0830047a9b5493d811d3737def
Nicole <nicole_a_alejandro@live.com>
2018-11-21 14:36:06 -0500

    Fixed extra closing tag

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
         <Person name = "Max" age = "28"/>
-        <Person name = "Jane" age = "30">My Hobbies: Racing</Person>/>
+        <Person name = "Jane" age = "30">My Hobbies: Racing</Person>
         <Person name = "Joe" age = "42"/>
       </div>
     );
